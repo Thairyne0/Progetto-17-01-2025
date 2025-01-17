d63489fcb23bb9bf5380f2ab09dd44730eb4e074
Tommaso Cesaro <to.cesaro@studenti.unina.it>
2025-01-17 11:25:16 +0100

    Start

---
+++ src/main/java/org/example/ElementoCatalogo.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public abstract class ElementoCatalogo {
+
+    private String isbn;
+    private String titolo;
+    private int annoPubblicazione;
+    private int numeroPagine;
+
+}
